6c85ca7ef64a1c4581a4fdd7578744357d7b0429
dangkhoaln <lndangkhoa19@gmail.com>
2023-08-13 16:59:48 +0700

    Modify solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,21 @@
-const removeFromArray = function(array, element) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === element) {
-            array.splice(i,1);
+const removeFromArray = function (array, ...elements) {
+    // for (let i = 0; i < array.length; i++) {
+    //     for (const element of elements) {
+    //         if (array[i] === element) {
+    //             array.splice(i,1);
+    //         }
+    //     }
+    // }
+    let i = 0;
+    while (i < array.length) {
+        let increase = true;
+        for (const element of elements) {
+            if (array[i] === element) {
+                array.splice(i, 1);
+                increase = false;
+            }
         }
+        if (increase) i+=1;
     }
     return array;
 };
