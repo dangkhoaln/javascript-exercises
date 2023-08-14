73512c66c3ab60489e91a333ebf4daa576043d20
dangkhoaln <lndangkhoa19@gmail.com>
2023-08-14 15:17:50 +0700

    Inital attempt

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!Number.isInteger(year) || year < 1) return 'ERROR';
+    if (year%4 !== 0) return false;
+    if (year%100 === 0 && year%400 !== 0) return false;
+    return true;
 };
 
 // Do not edit below this line
