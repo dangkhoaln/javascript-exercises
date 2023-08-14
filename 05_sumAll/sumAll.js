4682fcb2ed4e6d926c9de7f0861c1f262c1e674c
dangkhoaln <lndangkhoa19@gmail.com>
2023-08-14 15:04:26 +0700

    Attempt to solve after testing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,18 @@
-const sumAll = function(start, end) {
-    let sum = 0;
-    for (let i = start; i <= end; i++) {
-        sum += i;
+const sumAll = function (start, end) {
+    if (typeof (start) != "number"
+        || typeof (end) != "number"
+        || start < 0
+        || end < 0) {
+        return "ERROR";
     }
+    if (start > end) {
+        [start, end] = [end, start];
+    }
+    let sum = 0;
+    for (let i = start; i <= end; i++) { sum += i; }
     return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
